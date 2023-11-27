7822da65c5d2e37e0138d78d7d5b7d52e3b14b75
Ubuntu <ubuntu@ip-172-31-0-133.us-west-2.compute.internal>
2023-11-27 08:45:48 +0000

    modify unqiue and compact

--- python/dgl/graphbolt/internal/sample_utils.py
+++ python/dgl/graphbolt/internal/sample_utils.py
@@ -61,42 +61,42 @@ def unique_and_compact(
         return unique_and_compact_per_type(nodes)
 
 
-def unique_and_compact_node_pairs(
-    node_pairs: Union[
+def unique_and_compact_csc_formats(
+    csc_formats: Union[
         Tuple[torch.Tensor, torch.Tensor],
         Dict[str, Tuple[torch.Tensor, torch.Tensor]],
     ],
     unique_dst_nodes: Union[
         torch.Tensor,
         Dict[str, torch.Tensor],
-    ] = None,
+    ],
 ):
     """
-    Compact node pairs and return unique nodes (per type).
+    Compact csc formats and return unique nodes (per type).
 
     Parameters
     ----------
-    node_pairs : Union[Tuple[torch.Tensor, torch.Tensor],
-                    Dict(str, Tuple[torch.Tensor, torch.Tensor])]
-        Node pairs representing source-destination edges.
-        - If `node_pairs` is a tuple: It means the graph is homogeneous.
-        Also, it should be in the format ('u', 'v') representing source
-        and destination pairs. And IDs inside are homogeneous ids.
-        - If `node_pairs` is a dictionary: The keys should be edge type and
-        the values should be corresponding node pairs. And IDs inside are
-        heterogeneous ids.
+    csc_formats : Union[CSCFormatBase, Dict(str, CSCFormatBase)]
+        CSC formats representing source-destination edges.
+        - If `csc_formats` is a CSCFormatBase: It means the graph is
+        homogeneous. Also, indptr and indice in it should be torch.tensor
+        representing source and destination pairs in csc format. And IDs inside
+        are homogeneous ids.
+        - If `csc_formats` is a Dict[str, CSCFormatBase]: The keys
+        should be edge type and the values should be csc format node pairs.
+        And IDs inside are heterogeneous ids.
     unique_dst_nodes: torch.Tensor or Dict[str, torch.Tensor]
         Unique nodes of all destination nodes in the node pairs.
         - If `unique_dst_nodes` is a tensor: It means the graph is homogeneous.
-        - If `node_pairs` is a dictionary: The keys are node type and the
+        - If `csc_formats` is a dictionary: The keys are node type and the
         values are corresponding nodes. And IDs inside are heterogeneous ids.
 
     Returns
     -------
-    Tuple[node_pairs, unique_nodes]
-        The compacted node pairs, where node IDs are replaced with mapped node
+    Tuple[csc_formats, unique_nodes]
+        The compacted csc formats, where node IDs are replaced with mapped node
         IDs, and the unique nodes (per type).
-        "Compacted node pairs" indicates that the node IDs in the input node
+        "Compacted csc formats" indicates that the node IDs in the input node
         pairs are replaced with mapped node IDs, where each type of node is
         mapped to a contiguous space of IDs ranging from 0 to N.
 
@@ -104,21 +104,27 @@ def unique_and_compact_node_pairs(
     --------
     >>> import dgl.graphbolt as gb
     >>> N1 = torch.LongTensor([1, 2, 2])
-    >>> N2 = torch.LongTensor([5, 6, 5])
-    >>> node_pairs = {"n1:e1:n2": (N1, N2),
-    ...     "n2:e2:n1": (N2, N1)}
-    >>> unique_nodes, compacted_node_pairs = gb.unique_and_compact_node_pairs(
-    ...     node_pairs
+    >>> N2 = torch.LongTensor([5, 5, 6])
+    >>> unique_dst = {
+    ...     "n1": torch.LongTensor([1, 2]),
+    ...     "n2": torch.LongTensor([5, 6])}
+    >>> csc_formats = {
+    ...     "n1:e1:n2": CSCFormatBase(indptr=torch.tensor([0, 2, 3]),indices=N1),
+    ...     "n2:e2:n1": CSCFormatBase(indptr=torch.tensor([0, 1, 3]),indices=N2)}
+    >>> unique_nodes, compacted_csc_formats = gb.unique_and_compact_csc_formats(
+    ...     csc_formats, unique_dst
     ... )
     >>> print(unique_nodes)
     {'n1': tensor([1, 2]), 'n2': tensor([5, 6])}
-    >>> print(compacted_node_pairs)
-    {"n1:e1:n2": (tensor([0, 1, 1]), tensor([0, 1, 0])),
-    "n2:e2:n1": (tensor([0, 1, 0]), tensor([0, 1, 1]))}
+    >>> print(compacted_csc_formats)
+    {"n1:e1:n2": CSCFormatBase(indptr=torch.tensor([0,2,3]),
+                               indices=torch.tensor([0, 1, 1])),
+     "n2:e2:n1": CSCFormatBase(indptr=torch.tensor([0,1,3]),
+                               indices=torch.Longtensor([0, 0, 1]))}
     """
-    is_homogeneous = not isinstance(node_pairs, dict)
+    is_homogeneous = not isinstance(csc_formats, dict)
     if is_homogeneous:
-        node_pairs = {"_N:_E:_N": node_pairs}
+        csc_formats = {"_N:_E:_N": csc_formats}
         if unique_dst_nodes is not None:
             assert isinstance(
                 unique_dst_nodes, torch.Tensor
@@ -126,53 +132,41 @@ def unique_and_compact_node_pairs(
             unique_dst_nodes = {"_N": unique_dst_nodes}
 
     # Collect all source and destination nodes for each node type.
-    src_nodes = defaultdict(list)
-    dst_nodes = defaultdict(list)
-    for etype, (src_node, dst_node) in node_pairs.items():
-        src_type, _, dst_type = etype_str_to_tuple(etype)
-        src_nodes[src_type].append(src_node)
-        dst_nodes[dst_type].append(dst_node)
-    src_nodes = {ntype: torch.cat(nodes) for ntype, nodes in src_nodes.items()}
-    dst_nodes = {ntype: torch.cat(nodes) for ntype, nodes in dst_nodes.items()}
-    # Compute unique destination nodes if not provided.
-    if unique_dst_nodes is None:
-        unique_dst_nodes = {
-            ntype: torch.unique(nodes) for ntype, nodes in dst_nodes.items()
-        }
+    indices = defaultdict(list)
+    for etype, csc_format in csc_formats.items():
+        src_type, _, _ = etype_str_to_tuple(etype)
+        indices[src_type].append(csc_format.indices)
+    indices = {ntype: torch.cat(nodes) for ntype, nodes in indices.items()}
 
-    ntypes = set(dst_nodes.keys()) | set(src_nodes.keys())
+    ntypes = set(indices.keys())
     unique_nodes = {}
-    compacted_src = {}
-    compacted_dst = {}
-    dtype = list(src_nodes.values())[0].dtype
+    compacted_indices = {}
+    dtype = list(indices.values())[0].dtype
     default_tensor = torch.tensor([], dtype=dtype)
     for ntype in ntypes:
-        src = src_nodes.get(ntype, default_tensor)
+        indice = indices.get(ntype, default_tensor)
         unique_dst = unique_dst_nodes.get(ntype, default_tensor)
-        dst = dst_nodes.get(ntype, default_tensor)
         (
             unique_nodes[ntype],
-            compacted_src[ntype],
-            compacted_dst[ntype],
-        ) = torch.ops.graphbolt.unique_and_compact(src, dst, unique_dst)
+            compacted_indices[ntype],
+        ) = torch.ops.graphbolt.unique_and_compact(indice, unique_dst)
 
-    compacted_node_pairs = {}
+    compacted_csc_formats = {}
     # Map back with the same order.
-    for etype, pair in node_pairs.items():
-        num_elem = pair[0].size(0)
+    for etype, csc_format in csc_formats.items():
+        num_elem = csc_format.indices.size(0)
         src_type, _, dst_type = etype_str_to_tuple(etype)
-        src = compacted_src[src_type][:num_elem]
-        dst = compacted_dst[dst_type][:num_elem]
-        compacted_node_pairs[etype] = (src, dst)
-        compacted_src[src_type] = compacted_src[src_type][num_elem:]
-        compacted_dst[dst_type] = compacted_dst[dst_type][num_elem:]
+        indice = compacted_indices[src_type][:num_elem]
+        indptr = csc_format.indptr
+        compacted_csc_formats[etype] = CSCFormatBase(indptr=indptr, indices=indice)
+        compacted_indices[src_type] = compacted_indices[src_type][num_elem:]
 
     # Return singleton for a homogeneous graph.
     if is_homogeneous:
-        compacted_node_pairs = list(compacted_node_pairs.values())[0]
+        compacted_csc_formats = list(compacted_csc_formats.values())[0]
         unique_nodes = list(unique_nodes.values())[0]
 
-    return unique_nodes, compacted_node_pairs
+    return unique_nodes, compacted_csc_formats
 
 
 def compact_csc_format(
